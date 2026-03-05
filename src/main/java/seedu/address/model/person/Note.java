7c780f74960f8f7c6e1d8be683c4e9c10a73a967
ray <raychua0106@gmail.com>
2026-03-05 10:13:28 +0800

    Create Note.java

---
+++ src/main/java/seedu/address/model/person/Note.java
@@ -0,0 +1,34 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+
+public class Note {
+    public final String value;
+
+    public Note(String note) {
+        requireNonNull(note);
+        value = note;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if (!(other instanceof Note)) {
+            return false;
+        }
+        Note otherNote = (Note) other;
+        return value.equals(otherNote.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
