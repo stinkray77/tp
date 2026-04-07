6dfda46811115e9c8885bf014d311ccc665db4a0
ray <raychua0106@gmail.com>
2026-04-07 23:48:02 +0800

    Fix checkstyle errors

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -39,145 +39,145 @@ import seedu.address.testutil.PersonUtil;
 
 public class AddressBookParserTest {
 
-        private final AddressBookParser parser = new AddressBookParser();
+    private final AddressBookParser parser = new AddressBookParser();
 
-        @Test
-        public void parseCommand_add() throws Exception {
-                Person person = new PersonBuilder().build();
-                AddCommand command = (AddCommand) parser.parseCommand(PersonUtil.getAddCommand(person));
-                assertEquals(new AddCommand(person), command);
+    @Test
+    public void parseCommand_add() throws Exception {
+        Person person = new PersonBuilder().build();
+        AddCommand command = (AddCommand) parser.parseCommand(PersonUtil.getAddCommand(person));
+        assertEquals(new AddCommand(person), command);
 
-                AddCommand mixedCaseCommand = (AddCommand) parser
-                                .parseCommand("AdD" + PersonUtil.getAddCommand(person).substring(3));
-                assertEquals(new AddCommand(person), mixedCaseCommand);
-        }
+        AddCommand mixedCaseCommand = (AddCommand) parser
+                        .parseCommand("AdD" + PersonUtil.getAddCommand(person).substring(3));
+        assertEquals(new AddCommand(person), mixedCaseCommand);
+    }
 
-        @Test
-        public void parseCommand_clear() throws Exception {
-                assertTrue(parser.parseCommand(ClearCommand.COMMAND_WORD) instanceof ClearCommand);
-                assertTrue(parser.parseCommand(ClearCommand.COMMAND_WORD + " 3") instanceof ClearCommand);
-                assertTrue(parser.parseCommand("ClEaR") instanceof ClearCommand);
-        }
+    @Test
+    public void parseCommand_clear() throws Exception {
+        assertTrue(parser.parseCommand(ClearCommand.COMMAND_WORD) instanceof ClearCommand);
+        assertTrue(parser.parseCommand(ClearCommand.COMMAND_WORD + " 3") instanceof ClearCommand);
+        assertTrue(parser.parseCommand("ClEaR") instanceof ClearCommand);
+    }
 
-        @Test
-        public void parseCommand_delete() throws Exception {
-                DeleteCommand command = (DeleteCommand) parser.parseCommand(
-                                DeleteCommand.COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased());
-                assertEquals(new DeleteCommand(INDEX_FIRST_PERSON), command);
+    @Test
+    public void parseCommand_delete() throws Exception {
+        DeleteCommand command = (DeleteCommand) parser.parseCommand(
+                        DeleteCommand.COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased());
+        assertEquals(new DeleteCommand(INDEX_FIRST_PERSON), command);
 
-                DeleteCommand mixedCaseCommand = (DeleteCommand) parser.parseCommand(
-                                "DeLeTe " + INDEX_FIRST_PERSON.getOneBased());
-                assertEquals(new DeleteCommand(INDEX_FIRST_PERSON), mixedCaseCommand);
-        }
+        DeleteCommand mixedCaseCommand = (DeleteCommand) parser.parseCommand(
+                        "DeLeTe " + INDEX_FIRST_PERSON.getOneBased());
+        assertEquals(new DeleteCommand(INDEX_FIRST_PERSON), mixedCaseCommand);
+    }
 
-        @Test
-        public void parseCommand_edit() throws Exception {
-                Person person = new PersonBuilder().build();
-                EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder(person).build();
-                EditCommand command = (EditCommand) parser.parseCommand(EditCommand.COMMAND_WORD + " "
-                                + INDEX_FIRST_PERSON.getOneBased() + " "
-                                + PersonUtil.getEditPersonDescriptorDetails(descriptor));
-                assertEquals(new EditCommand(INDEX_FIRST_PERSON, descriptor), command);
+    @Test
+    public void parseCommand_edit() throws Exception {
+        Person person = new PersonBuilder().build();
+        EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder(person).build();
+        EditCommand command = (EditCommand) parser.parseCommand(EditCommand.COMMAND_WORD + " "
+                        + INDEX_FIRST_PERSON.getOneBased() + " "
+                        + PersonUtil.getEditPersonDescriptorDetails(descriptor));
+        assertEquals(new EditCommand(INDEX_FIRST_PERSON, descriptor), command);
 
-                EditCommand mixedCaseCommand = (EditCommand) parser
-                                .parseCommand("EdIt " + INDEX_FIRST_PERSON.getOneBased()
-                                                + " " + PersonUtil.getEditPersonDescriptorDetails(descriptor));
-                assertEquals(new EditCommand(INDEX_FIRST_PERSON, descriptor), mixedCaseCommand);
-        }
+        EditCommand mixedCaseCommand = (EditCommand) parser
+                        .parseCommand("EdIt " + INDEX_FIRST_PERSON.getOneBased()
+                                        + " " + PersonUtil.getEditPersonDescriptorDetails(descriptor));
+        assertEquals(new EditCommand(INDEX_FIRST_PERSON, descriptor), mixedCaseCommand);
+    }
 
-        @Test
-        public void parseCommand_exit() throws Exception {
-                assertTrue(parser.parseCommand(ExitCommand.COMMAND_WORD) instanceof ExitCommand);
-                assertTrue(parser.parseCommand(ExitCommand.COMMAND_WORD + " 3") instanceof ExitCommand);
-                assertTrue(parser.parseCommand("ExIt") instanceof ExitCommand);
-        }
+    @Test
+    public void parseCommand_exit() throws Exception {
+        assertTrue(parser.parseCommand(ExitCommand.COMMAND_WORD) instanceof ExitCommand);
+        assertTrue(parser.parseCommand(ExitCommand.COMMAND_WORD + " 3") instanceof ExitCommand);
+        assertTrue(parser.parseCommand("ExIt") instanceof ExitCommand);
+    }
 
-        @Test
-        public void parseCommand_find() throws Exception {
-                List<String> keywords = Arrays.asList("foo", "bar", "baz");
-                FindCommand command = (FindCommand) parser.parseCommand(
-                                FindCommand.COMMAND_WORD + " " + keywords.stream().collect(Collectors.joining(" ")));
-                assertEquals(new FindCommand(new NameContainsKeywordsPredicate(keywords)), command);
+    @Test
+    public void parseCommand_find() throws Exception {
+        List<String> keywords = Arrays.asList("foo", "bar", "baz");
+        FindCommand command = (FindCommand) parser.parseCommand(
+                        FindCommand.COMMAND_WORD + " " + keywords.stream().collect(Collectors.joining(" ")));
+        assertEquals(new FindCommand(new NameContainsKeywordsPredicate(keywords)), command);
 
-                FindCommand mixedCaseCommand = (FindCommand) parser.parseCommand(
-                                "FiNd " + keywords.stream().collect(Collectors.joining(" ")));
-                assertEquals(new FindCommand(new NameContainsKeywordsPredicate(keywords)), mixedCaseCommand);
-        }
+        FindCommand mixedCaseCommand = (FindCommand) parser.parseCommand(
+                        "FiNd " + keywords.stream().collect(Collectors.joining(" ")));
+        assertEquals(new FindCommand(new NameContainsKeywordsPredicate(keywords)), mixedCaseCommand);
+    }
 
-        @Test
-        public void parseCommand_help() throws Exception {
-                assertTrue(parser.parseCommand(HelpCommand.COMMAND_WORD) instanceof HelpCommand);
-                assertTrue(parser.parseCommand(HelpCommand.COMMAND_WORD + " 3") instanceof HelpCommand);
-                assertTrue(parser.parseCommand("HeLp") instanceof HelpCommand);
-        }
+    @Test
+    public void parseCommand_help() throws Exception {
+        assertTrue(parser.parseCommand(HelpCommand.COMMAND_WORD) instanceof HelpCommand);
+        assertTrue(parser.parseCommand(HelpCommand.COMMAND_WORD + " 3") instanceof HelpCommand);
+        assertTrue(parser.parseCommand("HeLp") instanceof HelpCommand);
+    }
 
-        @Test
-        public void parseCommand_list() throws Exception {
-                assertTrue(parser.parseCommand(ListCommand.COMMAND_WORD) instanceof ListCommand);
-                assertTrue(parser.parseCommand(ListCommand.COMMAND_WORD + " 3") instanceof ListCommand);
-                assertTrue(parser.parseCommand("LiSt") instanceof ListCommand);
-        }
+    @Test
+    public void parseCommand_list() throws Exception {
+        assertTrue(parser.parseCommand(ListCommand.COMMAND_WORD) instanceof ListCommand);
+        assertTrue(parser.parseCommand(ListCommand.COMMAND_WORD + " 3") instanceof ListCommand);
+        assertTrue(parser.parseCommand("LiSt") instanceof ListCommand);
+    }
 
-        @Test
-        public void parseCommand_remark() throws Exception {
-                RemarkCommand command = (RemarkCommand) parser.parseCommand(
-                                RemarkCommand.COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased() + " r/ hello");
-                assertEquals(new RemarkCommand(INDEX_FIRST_PERSON, new Remark("hello")), command);
+    @Test
+    public void parseCommand_remark() throws Exception {
+        RemarkCommand command = (RemarkCommand) parser.parseCommand(
+                        RemarkCommand.COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased() + " r/ hello");
+        assertEquals(new RemarkCommand(INDEX_FIRST_PERSON, new Remark("hello")), command);
 
-                RemarkCommand mixedCaseCommand = (RemarkCommand) parser.parseCommand(
-                                "ReMaRk " + INDEX_FIRST_PERSON.getOneBased() + " r/ hello");
-                assertEquals(new RemarkCommand(INDEX_FIRST_PERSON, new Remark("hello")), mixedCaseCommand);
-        }
+        RemarkCommand mixedCaseCommand = (RemarkCommand) parser.parseCommand(
+                        "ReMaRk " + INDEX_FIRST_PERSON.getOneBased() + " r/ hello");
+        assertEquals(new RemarkCommand(INDEX_FIRST_PERSON, new Remark("hello")), mixedCaseCommand);
+    }
 
-        @Test
-        public void parseCommand_view() throws Exception {
-                ViewCommand command = (ViewCommand) parser.parseCommand(
-                                ViewCommand.COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased());
-                assertEquals(new ViewCommand(INDEX_FIRST_PERSON), command);
+    @Test
+    public void parseCommand_view() throws Exception {
+        ViewCommand command = (ViewCommand) parser.parseCommand(
+                        ViewCommand.COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased());
+        assertEquals(new ViewCommand(INDEX_FIRST_PERSON), command);
 
-                ViewCommand mixedCaseCommand = (ViewCommand) parser.parseCommand(
-                                "ViEw " + INDEX_FIRST_PERSON.getOneBased());
-                assertEquals(new ViewCommand(INDEX_FIRST_PERSON), mixedCaseCommand);
-        }
+        ViewCommand mixedCaseCommand = (ViewCommand) parser.parseCommand(
+                        "ViEw " + INDEX_FIRST_PERSON.getOneBased());
+        assertEquals(new ViewCommand(INDEX_FIRST_PERSON), mixedCaseCommand);
+    }
 
-        @Test
-        public void parseCommand_unrecognisedInput_throwsParseException() {
-                assertThrows(ParseException.class,
-                                String.format(MESSAGE_INVALID_COMMAND_FORMAT, HelpCommand.MESSAGE_USAGE),
-                                () -> parser.parseCommand(""));
-        }
+    @Test
+    public void parseCommand_unrecognisedInput_throwsParseException() {
+        assertThrows(ParseException.class,
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, HelpCommand.MESSAGE_USAGE), ()
+                -> parser.parseCommand(""));
+    }
 
-        @Test
-        public void parseCommand_unknownCommand_throwsParseException() {
-                assertThrows(ParseException.class, MESSAGE_UNKNOWN_COMMAND,
-                                () -> parser.parseCommand("unknownCommand"));
-        }
+    @Test
+    public void parseCommand_unknownCommand_throwsParseException() {
+        assertThrows(ParseException.class, MESSAGE_UNKNOWN_COMMAND, ()
+                -> parser.parseCommand("unknownCommand"));
+    }
 
-        @Test
-        public void parseCommand_mark() throws Exception {
-                MarkCommand command = (MarkCommand) parser.parseCommand(
-                                MarkCommand.COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased() + " ps/Paid");
-                assertEquals(new MarkCommand(INDEX_FIRST_PERSON, new PaymentStatus("Paid")), command);
+    @Test
+    public void parseCommand_mark() throws Exception {
+        MarkCommand command = (MarkCommand) parser.parseCommand(
+                        MarkCommand.COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased() + " ps/Paid");
+        assertEquals(new MarkCommand(INDEX_FIRST_PERSON, new PaymentStatus("Paid")), command);
 
-                MarkCommand mixedCaseCommand = (MarkCommand) parser.parseCommand(
-                                "MaRk " + INDEX_FIRST_PERSON.getOneBased() + " ps/Paid");
-                assertEquals(new MarkCommand(INDEX_FIRST_PERSON, new PaymentStatus("Paid")), mixedCaseCommand);
-        }
+        MarkCommand mixedCaseCommand = (MarkCommand) parser.parseCommand(
+                        "MaRk " + INDEX_FIRST_PERSON.getOneBased() + " ps/Paid");
+        assertEquals(new MarkCommand(INDEX_FIRST_PERSON, new PaymentStatus("Paid")), mixedCaseCommand);
+    }
 
-        @Test
-        public void parseCommand_listAttendance() throws Exception {
-                ListAttendanceCommand command = (ListAttendanceCommand) parser.parseCommand(
-                                ListAttendanceCommand.COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased());
-                assertEquals(new ListAttendanceCommand(INDEX_FIRST_PERSON, null), command);
-        }
+    @Test
+    public void parseCommand_listAttendance() throws Exception {
+        ListAttendanceCommand command = (ListAttendanceCommand) parser.parseCommand(
+                        ListAttendanceCommand.COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased());
+        assertEquals(new ListAttendanceCommand(INDEX_FIRST_PERSON, null), command);
+    }
 
-        @Test
-        public void parseCommand_markAttendance() throws Exception {
-                MarkAttendanceCommand command = (MarkAttendanceCommand) parser.parseCommand(
-                                MarkAttendanceCommand.COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased()
-                                                + " s/Mathematics l/Lesson 1 st/Present");
-                assertEquals(new MarkAttendanceCommand(
-                                INDEX_FIRST_PERSON, "Mathematics", "Lesson 1", AttendanceStatus.PRESENT), command);
-        }
+    @Test
+    public void parseCommand_markAttendance() throws Exception {
+        MarkAttendanceCommand command = (MarkAttendanceCommand) parser.parseCommand(
+                        MarkAttendanceCommand.COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased()
+                                        + " s/Mathematics l/Lesson 1 st/Present");
+        assertEquals(new MarkAttendanceCommand(
+                        INDEX_FIRST_PERSON, "Mathematics", "Lesson 1", AttendanceStatus.PRESENT), command);
+    }
 
 }
